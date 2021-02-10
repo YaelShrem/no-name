05e2719418bce360806640a77db88909e093d871
ei0548488994 <*ei0548488994@gmail.com*>
2021-02-10 14:28:52 +0200

    esty

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
       <Header/>
       {/* <Login2 /> */}
       <BusinessDetails/>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/businessDetails/BusinessDetails.js
+++ src/components/businessDetails/BusinessDetails.js
@@ -9,22 +9,25 @@ export default function BusinessDetails() {
     <>
     <div>
       <div
-        className="d-flex justify-content-between"
+        className=""
         style={{ height: "30vh", minHeight: "800px", marginTop: "77px" }}
       >
+        <div style={{position:"relative"}}>
         <img
           src={backgroundGray}
           alt={backgroundGray}
-          className="col-9"
-          // style={{ height: "70vh" }}
-        />
-          <img src={imageFood} alt={imageFood} className="imageFood" />
-        
-      </div>
-      <div className="text d-flex align-items-end flex-column">
+          // className="col-9"
+          style={{ height: "560px",width:"1000px",    position: "absolute",
+          right:"0 ",padding:"0" }}
+        /><div className="text d-flex align-items-end flex-column">
         <p className="name p-2">BIGA</p>
         <p className="menu p-2">מטבח | ביקריי | קפה</p>
       </div>
+        </div>
+          <img src={imageFood} alt={imageFood} className="imageFood" />
+        
+      </div>
+      
    
       </div>
        </>
