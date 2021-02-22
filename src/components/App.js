1f1f38d51b7d1f9bd52d87f95d988bc4aa321224
ei0548488994 <*ei0548488994@gmail.com*>
2021-02-22 13:02:09 +0200

    esty

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import Header from "./header/Header";
-import HomePage from './homePage/HomePage';
+// import Header from "./header/Header";
+// import HomePage from './homePage/HomePage';
 //import Login2 from "./login/Login2";
 //import BusinessDetails from "./businessDetails/BusinessDetails";
 //import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
@@ -7,12 +7,12 @@ import HomePage from './homePage/HomePage';
 //import Recommendations from "./recommendations/Recommendations";
 //import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
 //import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
-import Login2 from "./login/Login2";
-import BusinessDetails from "./businessDetails/BusinessDetails";
+// import Login2 from "./login/Login2";
+// import BusinessDetails from "./businessDetails/BusinessDetails";
 import Footer from "./footer/Footer";
 //import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
 //import SearchSection from "./homePage/searchSection/SearchSection";
-import Recommendations from "./recommendations/Recommendations";
+// import Recommendations from "./recommendations/Recommendations";
 // import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
 // import Pricing from './homePage/pricing/Pricing';
 
