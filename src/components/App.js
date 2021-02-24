894090a9940b422bb4822b7ddf1d37330f0ce641
ei0548488994 <*ei0548488994@gmail.com*>
2021-02-24 12:07:29 +0200

    esty

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@
 //import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
 //import SearchSection from "./homePage/searchSection/SearchSection";
 //import Recommendations from "./recommendations/Recommendations";
-//import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
+// import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
 //import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
-// import Login2 from "./login/Login2";
+import Login2 from "./login/Login2";
 // import BusinessDetails from "./businessDetails/BusinessDetails";
-import Footer from "./footer/Footer";
+// import Footer from "./footer/Footer";
 //import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
 //import SearchSection from "./homePage/searchSection/SearchSection";
 // import Recommendations from "./recommendations/Recommendations";
@@ -23,11 +23,11 @@ function App() {
       {/* <HomePage/> */}
       {/* <Pricing/> */}
       {/* <Recommendations /> */}
-      {/* <Login2 /> */}
+      <Login2 />
       {/* <BusinessDetails /> */}
       {/* <OrderDetails /> */}
       {/* <ProductCatalog /> */}
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <div className="footer d-flex justify-content-center font-small pt-4">
       <div  className="container-footer d-flex justify-content-center align-items-center col-xl-10 col-lg-11" >
         <div className="wrapper-footer d-flex justify-content-between row">
-          <div className="col-xl-5 col-lg-6 col-md-6 col-sm-6">
+          <div className="col-xl-3 col-lg-6 col-md-12 col-sm-12">
             <h5 className="titleFooter" style={{color:"#B9B9B9"}}>Logo</h5>
             <div className="content">
                Xbiz.io מנוע חיפוש מקומי למציאת נקודות עניין, עסקים, אירועים
@@ -16,8 +16,8 @@ export default function Footer() {
              האלגוריתם של האתר והמידע בתוכו מתעדכנים באופן קבוע.
             </div>
           </div>
-          <div className="d-flex justify-content-between wrapper col-xl-7 col-lg-6 col-md-6 col-sm-6">
-            <div className="ulWrapper col-xl-5 col-lg-6 col-md-6 col-sm-6">
+          <div className="d-flex justify-content-between wrapper col-xl-7 col-lg-6 col-md-12 col-sm-12">
+            <div className="ulWrapper col-xl-5 col-lg-6 col-md-6 col-sm-12">
               <h5 className="titleFooter" style={{color:"#B9B9B9"}} >לינקים שימושיים</h5>
               <ul className="ul-footer d-flex flex-column justify-content-between">
                 <li className="list-unstyled">
@@ -40,7 +40,7 @@ export default function Footer() {
                 </li>
               </ul>
             </div>
-            <div className="col-xl-5 col-lg-6 col-md-6 col-sm-6">
+            <div className="col-xl-5 col-lg-6 col-md-6 col-sm-12">
               <h5 className="titleFooter" style={{color:"#B9B9B9"}}>יצירת קשר</h5>
               <div className="contact">
                 <p>בר כוכבא 4 קומה 6</p>
