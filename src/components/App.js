a3d897d8da48e666a47815f04950a31c1869392f
ei0548488994 <*ei0548488994@gmail.com*>
2021-02-28 12:04:01 +0200

    esty

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
- import Header from "./header/Header";
- import HomePage from './homePage/HomePage';
+//  import Header from "./header/Header";
+//  import HomePage from './homePage/HomePage';
 //import Login2 from "./login/Login2";
 //import BusinessDetails from "./businessDetails/BusinessDetails";
 //import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
@@ -7,7 +7,7 @@
 //import Recommendations from "./recommendations/Recommendations";
 //import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
 //import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
-// import Login2 from "./login/Login2";
+import Login2 from "./login/Login2";
 // import BusinessDetails from "./businessDetails/BusinessDetails";
 // import Footer from "./footer/Footer";
 // import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
@@ -17,23 +17,23 @@
 // import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
 // import Pricing from "./homePage/pricing/Pricing";
 // import MyShoppingBag from "./shoppingCart/myShoppingBag/MyShoppingBag";
-import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
+// import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
 // import UserMenu from "./shoppingCart/userMenu/UserMenu";
 
 function App() {
   return (
     <>
-      <Header />
-      <HomePage/>
+      {/* <Header /> */}
+      {/* <HomePage/> */}
       {/* <Pricing/> */}
       {/* <Recommendations /> */}
-      {/* <Login2 /> */}
+      <Login2 />
       {/* <BusinessDetails/> */}
       {/* <UserMenu /> */}
       {/* <OrderDetails /> */}
       {/* <ProductCatalog /> */}
       {/* <MyShoppingBag /> */}
-      <ProductMenu />
+      {/* <ProductMenu /> */}
       {/* <Footer /> */}
     </>
   );
