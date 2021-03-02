63bda3be4cac787d53b2812c011a13b0f968e510
git3151 <hannah@leader.codes>
2021-03-02 11:41:21 +0200

    chani

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,19 @@
-//  import Header from "./header/Header";
-//  import HomePage from './homePage/HomePage';
-//import Login2 from "./login/Login2";
-//import BusinessDetails from "./businessDetails/BusinessDetails";
-//import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
-//import SearchSection from "./homePage/searchSection/SearchSection";
-//import Recommendations from "./recommendations/Recommendations";
-//import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
-//import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
-import Login2 from "./login/Login2";
+// import Header from "./header/Header";
+// import HomePage from './homePage/HomePage';
+// import Login2 from "./login/Login2";
+// import BusinessDetails from "./businessDetails/BusinessDetails";
+// import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
+// import SearchSection from "./homePage/searchSection/SearchSection";
 // import BusinessDetails from "./businessDetails/BusinessDetails";
 // import Footer from "./footer/Footer";
 // import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
 // import SearchSection from "./homePage/searchSection/SearchSection";
 // import Recommendations from "./recommendations/Recommendations";
 // import OrderDetails from "./shoppingCart/orderDetails/OrderDetails";
-import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
+// import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
 // import Pricing from "./homePage/pricing/Pricing";
-// import MyShoppingBag from "./shoppingCart/myShoppingBag/MyShoppingBag";
-// import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
+import MyShoppingBag from "./shoppingCart/myShoppingBag/MyShoppingBag";
+import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
 // import UserMenu from "./shoppingCart/userMenu/UserMenu";
 
 function App() {
@@ -27,13 +23,13 @@ function App() {
       {/* <HomePage/> */}
       {/* <Pricing/> */}
       {/* <Recommendations /> */}
-      <Login2 />
+      {/* <Login2 /> */}
       {/* <BusinessDetails/> */}
       {/* <UserMenu /> */}
       {/* <OrderDetails /> */}
-      <ProductCatalog />
-      {/* <MyShoppingBag /> */}
-      {/* <ProductMenu /> */}
+      {/* <ProductCatalog /> */}
+      <MyShoppingBag />
+      <ProductMenu />
       {/* <Footer /> */}
     </>
   );
