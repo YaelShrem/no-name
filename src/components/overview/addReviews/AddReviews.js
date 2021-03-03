9ef0e01c6c3e616f36c70b8d830c9bf4fbdae661
git3151 <hannah@leader.codes>
2021-03-03 16:20:05 +0200

    c

--- src/components/overview/addReviews/AddReviews.js
+++ src/components/overview/addReviews/AddReviews.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./addReviews.css";
+import Parser from "html-react-parser";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Container ,Button} from "react-bootstrap";
+import Icon from"../../Icon";
 
 import UserMenu from "../../shoppingCart/userMenu/UserMenu";
 
@@ -17,7 +19,9 @@ export default function AddReviews() {
             What do you think about Biga
           </Form.Label>
           <Form.Control as="textarea" rows={3} className="textarea"/>
-          <Button className="button-uplaod-img" variant="primary" size="sm"></Button>
+          
+          <Button className="button-uplaod-img" variant="outline-warning" size="sm">add picture <Icon name="picture"/>
+        </Button>
         </Form>
         <Button className="btn-review" variant="outline-warning">Send Reviews</Button>
 
