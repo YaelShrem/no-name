012b2f864c0c58408f6b0bdce73926224fa99c9e
git3151 <hannah@leader.codes>
2021-03-09 11:36:50 +0200

    chani

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // import Header from "./header/Header";
 // import HomePage from './homePage/HomePage';
-import Login2 from "./login/Login2";
+// import Login2 from "./login/Login2";
 // import BusinessDetails from "./overview/businessDetails/BusinessDetails";
 // import TopBusinesses from "./homePage/topBusinesses/TopBusinesses";
 // import SearchSection from "./homePage/searchSection/SearchSection";
@@ -10,18 +10,19 @@ import Login2 from "./login/Login2";
 // import ProductCatalog from "./shoppingCart/productCatalog/ProductCatalog";
 // import Pricing from "./homePage/pricing/Pricing";
 // import MyShoppingBag from "./shoppingCart/myShoppingBag/MyShoppingBag";
-import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
-import UserMenu from "./shoppingCart/userMenu/UserMenu";
+// import ProductMenu from "./shoppingCart/productMenu/ProductMenu";
+// import UserMenu from "./shoppingCart/userMenu/UserMenu";
 // import AddReviews from "./overview/addReviews/AddReviews";
+import TabsTitle from "./tabsTitle/TabsTitle";
 
 function App() {
   return (
     <>
       {/* <Header /> */}
       {/* <HomePage/> */}
-      {/* <Pricing/> */}
+      {/* <Pricing /> */}
       {/* <Recommendations /> */}
-      <Login2 />
+      {/* <Login2 /> */}
       {/* <BusinessDetails/> */}
       {/* <UserMenu /> */}
       {/* <OrderDetails /> */}
@@ -29,6 +30,7 @@ function App() {
       {/* <MyShoppingBag /> */}
       {/* <ProductMenu /> */}
       {/* <AddReviews /> */}
+     <TabsTitle/>
       {/* <Footer /> */}
     </>
   );
