1a77b9410aad09c6f1c0f056c74d972ee3bc7e3c
git3151 <hannah@leader.codes>
2021-03-14 11:10:34 +0200

    chani

--- src/components/addListing/AddListing.js
+++ src/components/addListing/AddListing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./addListing.css";
-import { Container, Button, Form, Col, Row } from "react-bootstrap";
+import { Container, Button, Form, Col, Row ,Card} from "react-bootstrap";
 
 export default function AddListing() {
   return (
@@ -11,30 +11,43 @@ export default function AddListing() {
           <Button className="btn-add-listing" variant="primary">
             Add Listing
           </Button>
-          <section>
-          <Row>
-            <Col>
-            <Form.Group controlId="formGroupEmail">
-            <Form.Label className="listing-lable">Listing Title</Form.Label>
-            <Form.Control type="email" className="listing-control"/>
-          </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <Form.Group controlId="formGroupEmail">
-            <Form.Label className="listing-lable">Category</Form.Label>
-            <Form.Control type="email" className="listing-control"/>
-          </Form.Group>
-            </Col>
-            <Col>
-            <Form.Group controlId="formGroupEmail">
-            <Form.Label className="listing-lable">Keywords</Form.Label>
-            <Form.Control type="email" className="listing-control"/>
-          </Form.Group>
-            </Col>
-          </Row>
-          </section>
+          {/* <section> */}
+          <Card style={{ width: "57rem" }}>
+            <Card.Body className="body-add-listing">
+              <Card.Title className="basic-information">Basic Information</Card.Title>
+              <Card.Text>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="formGroupEmail">
+                      <Form.Label className="listing-lable">
+                        Listing Title
+                      </Form.Label>
+                      <Form.Control type="email" className="listing-control" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="formGroupEmail">
+                      <Form.Label className="listing-lable">
+                        Category
+                      </Form.Label>
+                      <Form.Control type="email" className="listing-control" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="formGroupEmail">
+                      <Form.Label className="listing-lable">
+                        Keywords
+                      </Form.Label>
+                      <Form.Control type="email" className="listing-control" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          {/* </section> */}
         </Form>
       </Container>
     </>
