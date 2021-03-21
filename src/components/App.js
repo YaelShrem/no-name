9d35995c300f4754a77d3eea3d5132b3ecfd59fc
ei0548488994 <*ei0548488994@gmail.com*>
2021-03-21 15:45:50 +0200

    esty

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 // import Header from "./header/Header";
-import HomePage from './homePage/HomePage';
+// import HomePage from './homePage/HomePage';
 // import Login2 from "./login/Login2";
-// import BusinessDetails from "./overview/businessDetails/BusinessDetails";
+import BusinessDetails from "./overview/businessDetails/BusinessDetails";
 // import TopBusinesses from "../components/homePage/topBusinesses/TopBusinessesSection";
 // import SearchSection from "./homePage/searchSection/SearchSection";
 // import Footer from "./footer/Footer";
@@ -30,11 +30,11 @@ function App() {
   return (
     <>
       {/* <Header /> */}
-      <HomePage />
+      {/* <HomePage /> */}
       {/* <Pricing /> */}
       {/* <Login2 /> */}
       {/* <Recommendations /> */}
-      {/* <BusinessDetails /> */}
+      <BusinessDetails />
       {/* <UserMenu /> */}
       {/* <OrderDetails /> */}
       {/* <ProductCatalog /> */}
--- src/components/recommendations/Recommendations.js
+++ src/components/recommendations/Recommendations.js
@@ -9,7 +9,19 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 
 export default function Recommendations(props) {
@@ -64,8 +76,27 @@ export default function Recommendations(props) {
   return (
     <>
       <div className="wrapperRecommendations d-flex flex-column justify-content-between align-items-center">
-         <Slider className="wrapper-slider-recommendations" {...settings}>
-          <div className="slide-recommendations">
+      <Swiper
+      spaceBetween={50}
+      navigation
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      breakpoints={{
+        760: {
+          slidesPerView:2,
+        },
+        1050: {
+          slidesPerView:3,
+        }, 
+        1500: {
+          slidesPerView: 4,
+        },
+        2000: {
+          slidesPerView: 5,
+        }
+      }}
+    >
+       <SwiperSlide>  <div className="slide-recommendations">
           <div className="item d-flex flex-column align-items-center" data-value="1">
       <div className="wrap">
         <img src={message} alt={message} className="message" />
@@ -87,8 +118,8 @@ export default function Recommendations(props) {
         <p className="avaterText">Amanda Robertson</p>
       </div>
     </div>
-          </div>
-          <div className="slide-recommendations">
+          </div></SwiperSlide>
+          <SwiperSlide>  <div className="slide-recommendations">
           <div className="item d-flex flex-column align-items-center" data-value="1">
       <div className="wrap">
         <img src={message} alt={message} className="message" />
@@ -100,7 +131,7 @@ export default function Recommendations(props) {
                 name="simple-controlled"
                 value={value}
                 readOnly={true}
-              />
+               />
             </Box>
           </div>
         </div>
@@ -110,8 +141,8 @@ export default function Recommendations(props) {
         <p className="avaterText">Amanda Robertson</p>
       </div>
     </div>
-          </div>
-          <div className="slide-recommendations">
+          </div></SwiperSlide>
+          <SwiperSlide>  <div className="slide-recommendations">
           <div className="item d-flex flex-column align-items-center" data-value="1">
       <div className="wrap">
         <img src={message} alt={message} className="message" />
@@ -123,7 +154,7 @@ export default function Recommendations(props) {
                 name="simple-controlled"
                 value={value}
                 readOnly={true}
-              />
+               />
             </Box>
           </div>
         </div>
@@ -133,8 +164,8 @@ export default function Recommendations(props) {
         <p className="avaterText">Amanda Robertson</p>
       </div>
     </div>
-          </div>
-          <div className="slide-recommendations">
+          </div></SwiperSlide>
+          <SwiperSlide>  <div className="slide-recommendations">
           <div className="item d-flex flex-column align-items-center" data-value="1">
       <div className="wrap">
         <img src={message} alt={message} className="message" />
@@ -146,7 +177,7 @@ export default function Recommendations(props) {
                 name="simple-controlled"
                 value={value}
                 readOnly={true}
-              />
+               />
             </Box>
           </div>
         </div>
@@ -156,8 +187,8 @@ export default function Recommendations(props) {
         <p className="avaterText">Amanda Robertson</p>
       </div>
     </div>
-          </div>
-          <div className="slide-recommendations">
+          </div></SwiperSlide>
+          <SwiperSlide>  <div className="slide-recommendations">
           <div className="item d-flex flex-column align-items-center" data-value="1">
       <div className="wrap">
         <img src={message} alt={message} className="message" />
@@ -169,30 +200,7 @@ export default function Recommendations(props) {
                 name="simple-controlled"
                 value={value}
                 readOnly={true}
-              />
-            </Box>
-          </div>
-        </div>
-      </div>
-      <div className="divAvaset d-flex">
-        <Avatar alt="Travis Howard" src={Avater} className="avater" />        
-        <p className="avaterText">Amanda Robertson</p>
-      </div>
-    </div>
-          </div>
-          <div className="slide-recommendations">
-          <div className="item d-flex flex-column align-items-center" data-value="1">
-      <div className="wrap">
-        <img src={message} alt={message} className="message" />
-        <div className="wrapperp d-flex flex-column justify-content-between">
-          <p>Great food service delicious and served on a level.</p>
-          <div className="star-wrapper">
-            <Box component="fieldset" className="fieldset-star" mt={3} borderColor="transparent">
-              <Rating
-                name="simple-controlled"
-                value={value}
-                readOnly={true}
-              />
+               />
             </Box>
           </div>
         </div>
@@ -202,8 +210,31 @@ export default function Recommendations(props) {
         <p className="avaterText">Amanda Robertson</p>
       </div>
     </div>
+          </div></SwiperSlide>
+          <SwiperSlide>  <div className="slide-recommendations">
+          <div className="item d-flex flex-column align-items-center" data-value="1">
+      <div className="wrap">
+        <img src={message} alt={message} className="message" />
+        <div className="wrapperp d-flex flex-column justify-content-between">
+          <p>Great food service delicious and served on a level.</p>
+          <div className="star-wrapper">
+            <Box component="fieldset" className="fieldset-star" mt={3} borderColor="transparent">
+              <Rating
+                name="simple-controlled"
+                value={value}
+                readOnly={true}
+               />
+            </Box>
           </div>
-        </Slider>
+        </div>
+      </div>
+      <div className="divAvaset d-flex">
+        <Avatar alt="Travis Howard" src={Avater} className="avater" />
+        <p className="avaterText">Amanda Robertson</p>
+      </div>
+    </div>
+          </div></SwiperSlide>
+          </Swiper>
       </div>
       {/* Fonts to support Material Design  */}
       <link
